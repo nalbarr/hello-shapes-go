a2f33fb82d63c69aeffb469b2e5423c07c9359f9
nalbarr <nalbarracin@gmail.com>
2019-09-28 10:31:55 -0500

    NA. Fix bad imports carry-over from hello-luke-go project.

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
+	foo "hello-shapes-go/foo"
+	shapes "hello-shapes-go/shapes"
 	"reflect"
-	foo "hello-luke-go/foo"
-	shapes "hello-luke-go/shapes"
 )
 
 type rectangle struct {
-	base float64
+	base   float64
 	height float64
 }
 
@@ -17,8 +17,8 @@ func (x rectangle) Area() float64 {
 }
 
 func getArea(x shapes.Shape) {
-	fmt.Printf("x.TypeOf(): %s\n", reflect.TypeOf(x) )
-	fmt.Printf("x.Area(): %f\n", x.Area() )
+	fmt.Printf("x.TypeOf(): %s\n", reflect.TypeOf(x))
+	fmt.Printf("x.Area(): %f\n", x.Area())
 }
 
 func main() {
@@ -26,14 +26,21 @@ func main() {
 
 	foo.Bar()
 
-	// imported structs and functions
+	// import constructs from external module
+	// i.e., structs, functions
 	square := shapes.Square{Side: 4.0}
 	circle := shapes.Circle{Radius: 3.0}
 	triangle := shapes.Triangle{Base: 3.0, Height: 5.0}
 
-	// local struct and functions
+	// local scope
+	// i.e,. struct and functions
 	rectangle := rectangle{base: 3.0, height: 5.0}
 
+	// NOTE: Functional thinking pattern
+	// - FP: function applied to data structure
+	// instead of
+	// - OOP: object instance + behavior/method call requiring state
+	// to be passed to private fields via constructor
 	getArea(square)
 	getArea(circle)
 	getArea(triangle)
